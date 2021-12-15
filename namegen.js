329c749624b60c4d29ce75706f3862e64bccba32
MrKing-dev <grekin@hasdhawks.org>
2021-12-15 13:26:05 -0500

    Used browserify to bundle node.js requirements for creating browser code

--- namegen.js
+++ namegen.js
@@ -1,24 +1,32 @@
-const http = require('http');
 const randomPuppy = require('random-puppy');
 const names = {
     first: ["Roaring", "Fearless", "Black", "Broad", "Great", "Mighty", "Clever", "Crazy", "Swift", "Thundering", "Rapid", "Graceful", "Bloody", "Grumpy", "Deep", "Towering", "Shadowy", "Dark"],
     last: ["Waters", "Oak", "Pine", "Boulder", "Crag", "Heath", "River", "Cavern", "Sky", "Bark", "Mount", "Fjord", "Wolf", "Corbie", "Ettercap", "Hoolet"]
 }
+let outputUrl = "";
 
 
 function getRandomInt(max) {
     min = 0;
     max = Math.floor(max);
     return Math.floor(Math.random() * max); //The maximum is exclusive and the minimum is inclusive
-  } 
+  }   
 
-  const firstName = names.first[getRandomInt(names.first.length)];
-  const lastName = names.last[getRandomInt(names.last.length)];
-  const wolfName = firstName + " " + lastName;
+  function createName(){
+    const firstName = names.first[getRandomInt(names.first.length)];
+    const lastName = names.last[getRandomInt(names.last.length)];
+    const wolfName = firstName + " " + lastName;
+    console.log(wolfName);
+    document.getElementById('name-gen-output').innerHTML = wolfName;
 
-  randomPuppy('EarthPorn')
+    randomPuppy('EarthPorn')
   .then(url => {
       console.log(url);
+      outputUrl = url;
+      console.log(`The output url is ${outputUrl}`);
+      document.body.style.backgroundImage = `url(${outputUrl})`;
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
   })
 
-  console.log(wolfName);
+  }
